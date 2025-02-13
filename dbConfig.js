5f9de4b45f83832297e5220db2467637af2fc28a
Sameer  Moyal <sameerab827@gmail.com>
2025-02-13 12:00:14 +0530

    sequlize file-structure setup

--- dbConfig.js
+++ dbConfig.js
@@ -1,13 +1,13 @@
-export const dbConfig ={
-    HOST:'localhost',
-    USER:'root',
-     PASSWORD:"sameer",
-    DB:'sequlizedb',
-    dialect:'mysql',
-    pool:{
-        max:5,
-        min:0,
-        acquire:3000,
-        idle:1000
-    }
+export const dbConfig={
+        HOST:"localhost",
+        USER:'root',
+        PASSWORD:"sameer",
+        DB:"sequlizedb",
+        dialect:"mysql",
+        pool:{
+            min:0,
+            max:5,
+            idle:3000,
+            acquire:10000,
+        }
 }
--- sequilezConfig.js
+++ sequilezConfig.js
@@ -1,34 +1,62 @@
 import { Sequelize } from "sequelize";
 import { dbConfig } from "./dbConfig.js";
 
-const sequelize =new Sequelize(
-    
-        dbConfig.DB,
-        dbConfig.USER,
-        dbConfig.PASSWORD,
-        {
-            host:dbConfig.HOST,
-            dialect:dbConfig.dialect,
-            pool:{
-                max:dbConfig.pool.max,
-                min:dbConfig.pool.min,
-                acquire:dbConfig.pool.acquire,
-                idle:dbConfig.pool.idle,
-            }
-        },
-       
-    
-)
 
+const sequelize=new Sequelize(
+   dbConfig.DB,
+   dbConfig.USER,
+   dbConfig.PASSWORD,
+   {
+    host:dbConfig.HOST,
+     dialect:dbConfig.dialect,
+     pool:{
+        max:dbConfig.pool.max,
+        min:dbConfig.pool.min,
+        acquire:dbConfig.pool.acquire,
+        idle:dbConfig.pool.idle
+     }
+   }
+)
 
 sequelize.authenticate()
 .then(()=>console.log('Connection has been established'))
-.catch(err=>{
-    console.error('Unable to connet to the database',err)
-})
+.catch(err=>console.error('error detect to Connect sequelize',err))
 
-await sequelize.sync({force:false});
+sequelize.sync({force:false}).then(()=>console.log("sync : sync done"))
 
-console.log('yes, sync done');
+export default sequelize
 
-export default sequelize;
+// import { Sequelize } from "sequelize";
+// import { dbConfig } from "./dbConfig.js";
+
+// const sequelize =new Sequelize(
+    
+//         dbConfig.DB,
+//         dbConfig.USER,
+//         dbConfig.PASSWORD,
+//         {
+//             host:dbConfig.HOST,
+//             dialect:dbConfig.dialect,
+//             pool:{
+//                 max:dbConfig.pool.max,
+//                 min:dbConfig.pool.min,
+//                 acquire:dbConfig.pool.acquire,
+//                 idle:dbConfig.pool.idle,
+//             }
+//         },
+       
+    
+// )
+
+
+// sequelize.authenticate()
+// .then(()=>console.log('Connection has been established'))
+// .catch(err=>{
+//     console.error('Unable to connet to the database',err)
+// })
+
+// await sequelize.sync({force:false});
+
+// console.log('yes, sync done');
+
+// export default sequelize;
